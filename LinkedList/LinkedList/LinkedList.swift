db39a14414204470d24230d8321feaa04d3e8494
Sirak Zeray <sirakzeray@gmail.com>
2019-12-21 13:59:20 -0500

    Added more functions to linked List , improved speed and space aswell

--- LinkedList/LinkedList/LinkedList.swift
+++ LinkedList/LinkedList/LinkedList.swift
@@ -9,17 +9,19 @@
 import Foundation
 
 
-struct LinkedList<Value : Equatable> {
+struct LinkedList<Value : Equatable>{
     var head : Node<Value>?
     var tail: Node<Value>?
     
     public init() {}
     
+    /// Checks Linked List Empty
     public var isEmpty : Bool {
         return head == nil
     }
 
     mutating func push(_ value : Value) {
+        copyNodes()
         head = Node(value: value, next: head)
         if tail == nil {
             tail = head
@@ -27,6 +29,7 @@ struct LinkedList<Value : Equatable> {
     }
     
     mutating func appened(_ value : Value) {
+        copyNodes()
         if isEmpty {
             push(value)
         } else {
@@ -36,6 +39,7 @@ struct LinkedList<Value : Equatable> {
     }
     
     public func node(at index : Int) -> Node<Value>? {
+        
         var currentNode  = head
         var currentIndex = 0
         
@@ -50,6 +54,7 @@ struct LinkedList<Value : Equatable> {
     @discardableResult
     public mutating func insert(_ value: Value,
                                 after node : Node<Value>) -> Node<Value> {
+        copyNodes()
         guard tail !== node else {
             appened(value)
             return tail!
@@ -59,7 +64,84 @@ struct LinkedList<Value : Equatable> {
         return node.next!
     }
     
+    @discardableResult
+    public mutating func pop() -> Value?{
+        defer {
+            head = head?.next
+            if isEmpty {
+                tail = nil
+            }
+        }
+            return head?.value
+    }
     
+    @discardableResult
+    public mutating func removeFromLast() -> Value?{
+    
+        guard let head = head else {
+            return nil
+        }
+        
+        guard head.next != nil else {
+            return pop()
+        }
+        var prev = head
+        var current = head
+        
+        while let next = current.next {
+            prev = current
+            current = next
+        }
+        
+        prev.next = nil
+        tail = prev
+        return current.value
+    }
+    
+    
+    @discardableResult
+    public mutating func remove(after node : Node<Value>) -> Value? {
+        defer {
+            if node.next === tail {
+                tail = node
+            }
+            node.next = node.next?.next
+        }
+        return node.next?.value
+    }
+    
+    public  func printInReverse(_ passefNode : Node<Value>?) {
+    
+        
+        while let node = passefNode {
+            printInReverse(node.next)
+            print(node.value)
+            return
+        }
+       
+    }
+    
+    @discardableResult
+    public mutating func reverseList(_ passdNode : Node<Value>?) -> LinkedList {
+         copyNodes()
+        while let node = passdNode {
+            let lastList = reverseList(node.next)
+            var link = LinkedList()
+            if lastList.head != nil {
+                if lastList.head?.next != nil{
+                    let previousNode :  Node<Value> = lastList.head!.next!
+                                   previousNode.next = Node(value: node.value)
+                                   link.head = Node<Value>(value: lastList.head!.value , next: previousNode)
+                } else {
+                link.head = Node<Value>(value: lastList.head!.value , next: Node(value: node.value))
+                }
+            } else {
+                link.head = Node(value: node.value)
+            }
+            return link
+        }
+        return LinkedList()
+    }
 }
 
 extension LinkedList : CustomStringConvertible {
@@ -70,6 +152,69 @@ extension LinkedList : CustomStringConvertible {
         }
         return String(describing: head)
      }
+    
+    fileprivate mutating func copyNodes() {
+        guard var oldNode = head else {return}
+        head = Node(value: oldNode.value)
+        
+        var newNode = head
+        
+        while let nextOldNode = oldNode.next {
+            newNode!.next = Node(value: nextOldNode.value)
+            newNode = newNode!.next
+            
+            
+            oldNode = nextOldNode
+        }
+        tail = newNode
+    }
  
  
+}
+
+
+extension LinkedList : Collection {
+ 
+    
+    func index(after i: LinkedList<Value>.Index) -> Index {
+       return Index(node: i.node?.next)
+    }
+    
+    public subscript(position: LinkedList<Value>.Index) -> Value {
+        return position.node!.value
+    }
+    
+    var endIndex: LinkedList<Value>.Index {
+        return Index(node: tail?.next)
+    }
+    
+    public var startIndex: Index {
+        return Index(node: head)
+    }
+    public struct Index : Comparable {
+        public static func < (lhs: LinkedList<Value>.Index, rhs: LinkedList<Value>.Index) -> Bool {
+            guard lhs != rhs else {
+                return false
+            }
+            let nodes = sequence(first: lhs.node) {
+                $0?.next
+            }
+            return nodes.contains{$0 === rhs.node}
+        }
+        
+        public static func == (lhs: LinkedList<Value>.Index, rhs: LinkedList<Value>.Index) -> Bool {
+            switch (lhs.node , rhs.node) {
+            case let (left? , right?):
+                return left.next === right.next
+            case (nil , nil):
+                return true
+            default:
+                return false
+            }
+        }
+        
+        public var node : Node<Value>?
+        
+        
+    }
 }
--- LinkedList/LinkedList/main.swift
+++ LinkedList/LinkedList/main.swift
@@ -24,10 +24,38 @@ linkedList.push(3)
 linkedList.push(2)
 linkedList.push(1)
 linkedList.appened(0)
-
+//
 var middleNode = linkedList.node(at: 1)
 linkedList.insert(14, after: middleNode!)
 
+
 print(linkedList)
+
+print("After Pop")
+linkedList.pop()
+print(linkedList)
+
+
+print("After Removelast")
+linkedList.removeFromLast()
+print(linkedList)
+
+var list2 = linkedList
+
+print(linkedList)
+print(list2)
+
+//COW 
+print("After appending 8 to list ")
+list2.appened(8)
+
+print(linkedList)
+print(list2)
+
+print("Reversing Linked List")
+
+list2.printInReverse(list2.head)
+let reversedLink = list2.reverseList(list2.head)
+print(reversedLink)
 
 
